b02b8b6e1d5b49da9dc665bc0ec831bf1a501539
James Hume <james.hume@gmail.com>
2013-12-08 19:00:50 +0000

    some temporary playing. adding the trips and books marks tabs to the landing page and getting rid of obsolete javascript in landing.js... this was previosuly moved to sessions.js

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,27 +1,20 @@
+require 'pp'
+
 class LandingController < ApplicationController
   def index
     #Geocoder::Result::Google
     if params[:location].blank?
       #TODO need to handle empty string search
     else
+      @trips = Trip.all
       @search_location = Geocoder.search(params[:location])
       @hash_coord = [{"lat" => @search_location.first.coordinates[0], "lng" => @search_location.first.coordinates[1]}]
+
+      dump = PP.pp(@trips, "")
+      logger.debug dump
     end
 
     render :index
 
-  end
-
-  def login
-    if current_user()
-      index()
-    else
-      respond_to do |format|
-        format.html
-      end
-    end
-end
-
-  def browse
   end
 end
